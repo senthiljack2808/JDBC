69025bb02b4b1324880c8fef6e43c2268a9476f0
Senthil Kumar <senthil@striim.com>
2024-06-17 12:02:16 +0530

    enhancing the readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # JDBC
+
+* Load the driver class using class.forname()
+* Establish a connection
+* create statement
+* Execute Query
+* close connection
+
+
+In a real world imagine
+
+* Making a phone call
+* First it will establish a connection to your friend
+* initiate a conversation
+* speak for hrs
+* cut/end the call
--- crud/src/main/java/org/example/Statement.java
+++ crud/src/main/java/org/example/Statement.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Statement {
+   
+    public static void main(String[] args) throws  SQLException {
+        Connection con= DriverManager.getConnection("");
+        Statement statement= (Statement) con.createStatement();
+    }
 }
